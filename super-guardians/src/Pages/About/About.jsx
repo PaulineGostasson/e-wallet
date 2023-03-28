3ba9949b24bba30562c93e66b3848e9fd3f71125
PaulineGostasson <gostassonpauline@gmail.com>
2023-03-28 10:15:51 +0200

    About pagen

--- super-guardians/src/Pages/About/About.jsx
+++ super-guardians/src/Pages/About/About.jsx
@@ -1,6 +1,6 @@
 import "./About.css";
 import Header from "../../Components/Header/Header";
-import Footer from "../../Components/Footer/Footer"
+import Footer from "../../Components/Footer/Footer";
 
 function About() {
   return (
